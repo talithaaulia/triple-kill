bb7f0b3045c101c58c263e548013d0f554526781
talithaaulia <talithaali88@gmail.com>
2024-12-10 11:48:51 +0700

    coba

--- crudbuku/create.php
+++ crudbuku/create.php
@@ -21,7 +21,7 @@ class BookForm {
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Tambah Buku Baru</title>
+            <title>Tambah Buku</title>
             <style>
                 body {
                     font-family: Arial, sans-serif;
